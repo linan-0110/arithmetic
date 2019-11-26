0ca89fb21b8fb1dcc8a20c71e60dc505a03a3dce
linan-0110 <1597711363@qq.com>
2019-11-26 16:33:02 +0800

    新增聊天室

---
+++ 02_node/03.chatroom/app.js
@@ -0,0 +1,105 @@
+const Koa = require('koa')
+const bodyParser = require('koa-bodyparser')
+const static = require('koa-static')
+const render = require('koa-art-template')
+const Router = require('koa-router')
+const session = require('koa-session')
+const path = require('path')
+
+
+let app = new Koa()
+let router = new Router()
+
+// 加入socket.io
+const IO = require( 'koa-socket' )
+ 
+const io = new IO()
+ 
+// 附加到app上
+io.attach( app )
+ 
+io.on( 'connection', ( context, data ) => {
+  console.log( 'socket-server连接上！', data )
+  // 广播发送数据
+  io.broadcast('msg1', {msg: "socket-server"})
+})
+
+
+
+
+
+
+/* render 配置 */
+render(app, {
+    // render 查找页面的目录
+    root: path.join(__dirname, 'views'),
+    // 查找的后缀名
+    extname: '.html',
+    debug: process.env.NODE_ENV !== "production"
+})
+
+let msgs = [
+    { username: '小魔女', content: '你过来呀！！！' },
+    { username: '大大', content: 'wwwww！！！' },
+    { username: '呃呃', content: '333333' },
+    { username: '刚刚', content: 'ccccc' },
+    { username: '烦烦烦', content: '33333' }
+]
+
+/* ---------- start ---------- */
+router.get('/', (ctx) => {
+    ctx.render('index')
+})
+    .post('/login', (ctx) => {
+        let { username, password } = ctx.request.body
+        ctx.session.user = { username }
+
+        ctx.redirect('/list')
+    })
+    .get('/list', (ctx) => {
+        ctx.render('list', { username: ctx.session.user.username, msgs })
+    })
+    /* .post('/addNewContent', (ctx) => {
+        let username = ctx.session.user.username || '用户未登录'
+        let { newContentText } = ctx.request.body
+        msgs.push({ username, content: newContentText })
+        ctx.body = msgs
+    }) */
+
+
+/* ---------- end ---------- */
+
+
+/* 存储session */
+const store = {
+    myStore: {},
+    set(key, session) {
+        this.myStore[key] = session
+    },
+    get(key) {
+        return this.myStore[key]
+    },
+    destroy(key) {
+        delete this.myStore[key]
+    }
+}
+
+/* static 配置 */
+app.use(static('/public'))
+
+/* bodyParser 配置 */
+app.use(bodyParser());
+
+/* session 配置 */
+app.keys = ["test"]
+app.use(session({ store }, app))
+
+/* router 配置 */
+app.use(router.routes())
+
+/* 处理 status 405 501 */
+app.use(router.allowedMethods())
+
+app.listen(8888, () => {
+    console.log("服务器启动在：loaclhost:8888");
+})
